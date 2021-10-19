2a038da03bd3bcbad24596aa3d499609b790d7f9
aseani miller <milleraseani@gmail.com>
2021-10-19 14:46:40 -0700

    correction

--- src/components/github.js
+++ src/components/github.js
@@ -26,7 +26,7 @@ export default class Git extends Component {
                        title:'Using Recursion to transform integers into binary',
                        url: 'https://media-exp1.licdn.com/dms/image/C5622AQEVy37SokS-Eg/feedshare-shrink_800/0/1633274135894?e=1637193600&v=beta&t=BEvrVWoTcHkIw7oQG9vxuB4wfavPOHpKGjT7f_8-aCw',
                        desc:'In the above code we are utilizing Recursion to transform a list of integers into its binary counter part, as you can see we first check the type of datastructure it is first before we decide to convert it enabling us to be able transform multidimensional lists as well ',
-                       time_comp: '3 * O(n) as we have three loops neither of them are nested',
+                       time_comp: ' O(n)  + O(Logn)',
                        learned:  'Math library is not needed in this equation, I initially used it to calculate the length of the integers binary format using binary logarithm.  Log(integer) // 1 + 1 ',
                        output: 'https://i.imgur.com/e5aS799.jpg'
                        },
